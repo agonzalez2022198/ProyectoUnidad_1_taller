12083c7c549ea644862b1e1697b91808ca016727
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-02-18 20:10:52 -0600

    Modelo de usuarios

---
+++ models/usuario.js
@@ -0,0 +1,46 @@
+const { Schema, model} = require('mongoose');
+
+//Si fuera necesario, cambiaré las variables
+
+const UsuarioSchema = Schema({
+    nombre:{
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo:{
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img:{
+        type: String
+    },
+    role:{
+        type: String,
+        require: true,
+        enum: ["ADMIN_ROLE", "USER_ROLE"]
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    },
+    google:{
+        type: Boolean,
+        default: false
+    }
+
+
+});
+
+
+UsuarioSchema.methods.toJSON = function(){
+    const{ __v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+}
+
+module.exports = model('Usuario', UsuarioSchema);
