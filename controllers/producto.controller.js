bbd537fe590c376898d5659116419f66bb254926
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-02-20 16:34:09 -0600

    Controlador

---
+++ controllers/producto.controller.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcrypt');
+const Producto = require('../models/producto.model');
+const { response } = require('express');
+
+const productsGet = async (req, res = response) =>{
+
+}
+
+const getProductsById = async (req, res) => {
+
+}
+
+const putProducts = async (req, res = response)=>{
+    
+}
