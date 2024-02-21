bb3dcbcd4a5bbdfed40593d5fe9400b416b4aebd
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-02-21 16:52:13 -0600

    Agregadps

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -2,22 +2,84 @@ const bcrypt = require('bcrypt');
 const Producto = require('../models/producto.model');
 const { response } = require('express');
 
-const productsGet = async (req, res = response) =>{
+const productosGet = async (req, res = response) => {
+    const {limite, desde} = req.query;
+    const query = {estado: true};
 
+
+    const [total, producto] = await Promise.all([
+        Producto.countDocuments(query),
+        Producto.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
+
+
+    res. status(200).json({
+        total,
+        producto
+    });
 }
 
-const getProductsById = async (req, res) => {
+const getProductosById = async (req, res = response) => {
+    const {id} = req.params;
+    const producto = await Usuario.findOne({_id: id});
 
+    res.status(200).json({
+        producto
+    });
 }
 
-const putProducts = async (req, res = response)=>{
 
+const putProductos = async (req, res = response) =>{
+    const { id } = req.params;
+    const {_d, ...resto } = req.body;
+
+    //if(password){
+    //    const salt = bcryptjs.genSaltSync();
+    //    resto.password = bcryptjs.hashSync(password, salt);
+    //}
+
+    await Producto.findByIdAndUpdate(id, resto);
+
+    const producto = Producto.findOne({id});
+
+    res.status(200).json({
+        msg: 'Producto Actualizado Exitosamente, parce',
+        producto
+    });
 }
 
+const productosDelete = async (req, res) => {
+    const {id} = req.params;
+    const producto = await Producto.findByIdAndUpdate(id, {estado: false});
+    //const usuarioAutenticado = req.usuario;
+
+    res.status(200).json({
+        msg: 'Se elimino el producto',
+        producto
+    });
+}
+
+const productosPost = async (req, res) => {
+    const{nameProduct, descripcion, categoria, precio, stock} = req.body;
+    const producto = new Producto({nameProduct, descripcion, categoria, precio, stock});
+
+
+    //const salt = bcryptjs.genSaltSync();
+    //producto.password = bcryptjs.hashSync(password, salt);
+
+    await producto.save();
+    res.status(202).json({
+        producto
+    });
+}
 
 
 module.exports = {
-    getProductsById,
-    productsGet,
-    putProducts
+    productosGet,
+    getProductosById,
+    putProductos,
+    productosDelete,
+    productosPost
 }
