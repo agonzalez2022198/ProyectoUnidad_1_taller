5fe89bbb96e767275e33c3ef34bfe1106638ffb6
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-02-22 02:23:27 -0600

    Modelo para las facturas

---
+++ models/factura.model.js
@@ -0,0 +1,38 @@
+const { Schema, model} = require('mongoose');
+
+const FacturaSchema = Schema({
+
+    numeroFactura: {
+        type: String,
+        required: [true, 'El número es automático']
+    },
+
+    fechaEmision: {
+        type: String,
+        required: [true, 'Tomar la fecha de la pc']
+    },
+
+    cliente: {
+        type: String,
+        required: [true, 'Cliente que está comprando']
+    },
+
+    producto: {
+        type: String,
+        required: [true, 'Productos en el carrito']
+    },
+
+    detaller: {
+        type: String,
+        required: [true, 'Detalles de la factura']
+    },
+
+    estado: {
+        type: Boolean,
+        default: true
+    }
+
+});
+
+
+module.exports = model('Factura', FacturaSchema);
